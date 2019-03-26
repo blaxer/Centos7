48e7c0a3e1a2c3eaef283bc49adfb5e08c4df398
blaxer <jferrara@earthcompatible.com>
2019-03-26 08:13:38 -0700

    Update serversetup.sh

--- serversetup.sh
+++ serversetup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#This is a simple script used to setup centos7 VM's for testing
+#This is a simple setup script used for centos7 test VM's
 
 #install net tools
 yum install net-tools -y
@@ -16,21 +16,15 @@ systemctl stop firewalld
 systemctl disable firewalld
 
 #Kill selinux
-
 sudo sed -i 's/enforcing/disabled/g' /etc/selinux/config /etc/selinux/config
 
-
 #Setup automatic yum updates
-
 yum -y install yum-cron
-
 systemctl enable yum-cron.service
-
 systemctl start yum-cron.service
 
 
 #Install docker CE
-
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum install -y docker-ce
@@ -38,16 +32,12 @@ systemctl enable docker
 systemctl start docker
 
 #Install docker compose
-
 sudo yum install epel-release -y
 sudo yum install -y python-pip 
 sudo pip install docker-compose -y
-sudo yum upgrade python*
+sudo yum upgrade python* -y
 
 #Install portainer
-
 docker volume create portainer_data
 docker run -d --restart always --name Portainer -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
-
-
 
